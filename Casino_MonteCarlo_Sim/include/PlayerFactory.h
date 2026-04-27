338f76f6371c04530ab84a03c7a8d6e78d6d9a49
sangle-13 <lethanhsang2k7vn@gmail.com>
2026-04-27 21:27:22 +0700

    Add  PlayerFactory.h

--- Casino_MonteCarlo_Sim/include/PlayerFactory.h
+++ Casino_MonteCarlo_Sim/include/PlayerFactory.h
@@ -3,3 +3,21 @@
 #include "Strategies.h"
 #include <memory>
 #include <string>
+
+class PlayerFactory {
+public:
+    enum class Type { CHUNG_THUY, RAI_THAM };
+
+    static std::unique_ptr<Player> createPlayer(Type type, const std::string& name, double balance) {
+        double defaultBaseBet = 100.0;
+        std::unique_ptr<BettingStrategy> strat;
+
+        switch (type) {
+        case Type::CHUNG_THUY:
+            strat = std::make_unique<ChungThuyStrategy>(); break;
+        case Type::RAI_THAM:
+            strat = std::make_unique<RaiThamStrategy>(); break;
+        }
+        return std::make_unique<Player>(name, balance, defaultBaseBet, std::move(strat));
+    }
+};
