acfa6f3ad77eaf2cfe5755a77e3c86db6cf8013d
minhtran-debug <nhatminh06032007@gmail.com>
2026-04-28 17:06:58 +0200

    House.h

--- Casino_MonteCarlo_Sim/include/House.h
+++ Casino_MonteCarlo_Sim/include/House.h
@@ -14,9 +14,35 @@ public:
     // viết hàm getInitialBalance lấy vốn ban đầu nhà cái
     // viết hàm updateBalance cập nhật số vốn nhà cái
     // viết hàm isBankrupt kiểm tra nhà cái phá sản chưa
-    /*
     
+    // Lấy tên nhà cái
+    const std::string& getName() const {
+        return name;
+    }
+
+    // Vốn hiện tại
+    double getBalance() const {
+        return balance;
+    }
+
+    // Vốn ban đầu
+    double getInitialBalance() const {
+        return initialBalance;
+    }
+
+    // Cập nhật vốn (dương: lời, âm: lỗ)
+    void updateBalance(double amount) {
+        balance += amount;
+
+        // Nếu muốn chặt chẽ hơn (không cho âm):
+        if (balance < 0) {
+            balance = 0;
+        }
+    }
+
+    // Kiểm tra phá sản
+    bool isBankrupt() const {
+        return balance <= 0;
+    }
     
-    
-    */
 };
