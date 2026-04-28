72c571450c1d542c2b881eb3a50fdda43be3d832
minhtran-debug <nhatminh06032007@gmail.com>
2026-04-28 17:18:28 +0200

    Player.h

--- Casino_MonteCarlo_Sim/include/Player.h
+++ Casino_MonteCarlo_Sim/include/Player.h
@@ -19,8 +19,8 @@ public:
     //viết hàm getName (lấy tên người chơi) getBalance (lấy số vốn hiện tại)
     //Viết hàm getBets trả về 1 std::map (của cược, số tiền) người chơi sẽ đánh trong ván kế tiếp
     // viết hàm updateBalance để update vốn người chơi
-//   
 // Lấy tên người chơi
+//
     const std::string& getName() const {
         return name;
     }
